85aace0f314b7a204dc1fd93a30af57298f50075
Kiran Bharadwaj H D <hdkiranbharadwaj@gmail.com>
2024-04-15 20:44:45 +0530

    push to github

---
+++ queries.sql
@@ -0,0 +1,33 @@
+
+-- EXERCISE SOLUTION AND SETUP --
+
+DROP TABLE IF EXISTS visited_countries, users;
+
+CREATE TABLE users(
+id SERIAL PRIMARY KEY,
+name VARCHAR(15) UNIQUE NOT NULL,
+color VARCHAR(15)
+);
+
+CREATE TABLE visited_countries(
+id SERIAL PRIMARY KEY,
+country_code CHAR(2) NOT NULL,
+user_id INTEGER REFERENCES users(id)
+);
+
+INSERT INTO users (name, color)
+VALUES ('Angela', 'teal'), ('Jack', 'powderblue');
+
+INSERT INTO visited_countries (country_code, user_id)
+VALUES ('FR', 1), ('GB', 1), ('CA', 2), ('FR', 2 );
+
+SELECT *
+FROM visited_countries
+JOIN users
+ON users.id = user_id;
+
+CREATE TABLE countries (
+id serial PRIMARY KEY,
+country_code char(2),
+country_name varchar(100)
+);
